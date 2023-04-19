b53b35fe9e7dcd41515c3d29cbc8770021c7a510
KRiMZ0001 <130838241+KRiMZ0001@users.noreply.github.com>
2023-04-19 12:26:46 +0800

    Update EggFarm w ServerHop.lua

--- EggFarm w ServerHop.lua
+++ EggFarm w ServerHop.lua
@@ -1,20 +1,22 @@
-local GameId = 2788229376 
-local TeleportService = game:GetService("TeleportService")
-local Players = game:GetService("Players")
+repeat wait() until game:IsLoaded()
+
+local serverSizeThreshold = 30
+local rejoinseconds = 15
+local Servers = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/2788229376/servers/Public?sortOrder=Asc&limit=100"))
 
 while true do
-    Players.PlayerAdded:Wait()
+    for i, v in pairs(game:GetService("Workspace").Ignored:GetChildren()) do
+        if v:IsA("MeshPart") and v.Name ~= "Part" then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+            wait(1)
+        end
+    end
 
-    TeleportService:Teleport(GameId)
+    wait(rejoinseconds)
 
-    repeat wait() until game:IsLoaded()
-
-    while true do wait()
-        for i, v in pairs(game:GetService("Workspace").Ignored:GetChildren()) do
-            if v:IsA("MeshPart") then
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
-                wait(1)
-            end
+    for i,v in pairs(Servers.data) do
+        if v.playing < v.maxPlayers - serverSizeThreshold then
+            game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, v.id)
         end
     end
 end
